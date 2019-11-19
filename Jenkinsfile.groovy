8897688345e7dfdd14a40439b98c407b56c234a8
Thomas Khalil <thomas@khalilt.com>
2019-11-19 19:32:07 +0100

    codebuild simpler

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,10 @@
 pipeline {
-  agent {
-    label 'codebuilder'
-    docker { image 'node:7-alpine' }
-  }
-  stages {
-    stage('Build') {
-      steps {
-        sh 'node --version'
-      }
+    agent { docker { image 'node:6.3' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'npm --version'
+            }
+        }
     }
-  }
 }
